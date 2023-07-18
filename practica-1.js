836067a6c1a6342ba911bd6f56974deb3c6a37ec
Rafael Gracia <djrafael1004@gmail.com>
2023-07-17 21:39:12 -0500

    comit por error

--- practica-1.js
+++ practica-1.js
@@ -1,39 +1,38 @@
-// Lista de contactos predefinidos
+/// Lista de contactos predefinidos
 let contactos = [
-    "John Doe",
-    "Jane Smith",
-    "Michael Johnson",
-    "Emily Davis",
-  ];
-  
-  // Función para añadir un nuevo contacto a la lista
-  function agregarContacto(nombre) {
-    contactos.push(nombre);
+  "John Doe",
+  "Jane Smith",
+  "Michael Johnson",
+  "Emily Davis",
+];
+
+// Función para añadir un nuevo contacto a la lista
+function agregarContacto(nombre) {
+  contactos.push(nombre);
+}
+
+// Función para borrar un contacto existente de la lista
+function borrarContacto(nombre) {
+  let indice = contactos.indexOf(nombre);
+  if (indice !== -1) {
+    contactos.splice(indice, 1);
   }
-  
-  // Función para borrar un contacto existente de la lista
-  function borrarContacto(nombre) {
-    let indice = contactos.indexOf(nombre);
-    if (indice !== -1) {
-      contactos.splice(indice, 1);
-    }
+}
+
+// Función para imprimir los contactos en la consola
+function imprimirContactos() {
+ alert("Lista de contactos:");
+  for (let i = 0; i < contactos.length; i++) {
+    alert(contactos[i]);
   }
-  
-  // Función para imprimir los contactos en la consola
-  function imprimirContactos() {
-   alert("Lista de contactos:");
-    for (let i = 0; i < contactos.length; i++) {
-      alert(contactos[i]);
-    }
-  }
-  
-  // Ejemplo de uso
-  agregarContacto("Sarah Johnson");
-  agregarContacto("Robert Smith");
-  
-  imprimirContactos();
-  
-  borrarContacto("John Doe");
-  
-  imprimirContactos();
-  
+}
+
+// Ejemplo de uso
+agregarContacto("Sarah Johnson");
+agregarContacto("Robert Smith");
+
+imprimirContactos();
+
+borrarContacto("John Doe");
+
+imprimirContactos();
